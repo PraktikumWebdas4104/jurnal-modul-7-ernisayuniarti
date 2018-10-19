0f4e787b1fe0188a64c0adb51224d1177cc49ecb
ERNISA YUNIARTI SATRIYO PUTRI <43512652+ernisayuniarti@users.noreply.github.com>
2018-10-19 10:03:05 +0700

    Add files via upload
    ERNISA YUNIARTI SATRIYO PUTRI
    6701174038
    D3MI-41-04

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+
+	$server = "localhost";
+	$username = "root";
+	$password = "";
+	$db = "mahasiswa_db";
+
+	$koneksi = mysqli_connect($server, $username, $password, $db);
+
+	if (!$koneksi) {
+		echo "Koneksi Gagal";
+	}
+
+?>
